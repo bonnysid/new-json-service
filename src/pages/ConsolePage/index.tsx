d7d8df0b7d2f58f731b4fdcc307b7bbbc803433d
Nikita Bortsov <n.borcov@profsoft.pro>
2021-11-10 18:42:22 +0400

    [WEB-4] add check repeatable queries

--- src/pages/ConsolePage/index.tsx
+++ src/pages/ConsolePage/index.tsx
@@ -4,18 +4,28 @@ import QueryResponseBlock from 'components/QueryResponseBlock';
 import { ISendsayRequest } from 'api/console';
 import Layout from 'components/Layout';
 import { useSendQuery } from 'hooks/useSendQuery';
+import { useTypedSelector } from 'hooks/useTypedSelector';
 
 const ConsolePage = () => {
+    const history = useTypedSelector(state => state.console.history);
     const [query, setQuery] = useState<ISendsayRequest>({ action: 'pong' });
     const { response, isError, error, isLoading, executeQuery } = useSendQuery();
 
     const handleSendClick = () => {
-        executeQuery(query);
+        if (checkQuery()) {
+            executeQuery(query);
+        }
     };
 
     const onExecuteQuery = (query: ISendsayRequest) => {
         setQuery(query);
         executeQuery(query);
+    };
+
+    const checkQuery = () => {
+        const strHistory = history.map(query => JSON.stringify(query));
+        const strQuery = JSON.stringify(query);
+        return !strHistory.includes(strQuery);
     };
 
     return (
